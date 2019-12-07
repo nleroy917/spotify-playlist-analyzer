febb33abcbef01943c4ce1f5d05adda5050018fa
Nathan LeRoy <NLeRoy917@gmail.com>
2019-12-06 21:42:00 -0500

    Update git ignore, add playlist frameork, set up main script

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 #! bin/bash/python3
 
 from lib.authorize import *
+from lib.playlists import *
 
 
 if __name__ == '__main__':
-
-	playlist_URI = 'spotify:playlist:7iGY7WPiTvvSTr6ZgO3rR2'
-	playlist_share_link = 'https://open.spotify.com/playlist/7iGY7WPiTvvSTr6ZgO3rR2?si=iDvebMC4Q8Kderz6HyegOA'
 
 	spotify_authenticator = Authenticator('testing/configs/config.ini')
 	spotify_authenticator.authorize()
 	auth_header = spotify_authenticator.generate_header()
 
 	username = 'NLeRoy917'
-	response = requests.get('https://api.spotify.com/v1/users/{}/playlists'.format(username),
-                            headers=auth_header)
+	wrapped_2019 = 'https://open.spotify.com/playlist/37i9dQZF1Ethb70Ir9WW6o?si=yrv4GDu6SBeUc8PexoZ_HQ'
 
-	playlists = json.loads(response.text)
+	playlist_id = get_playlist_id(wrapped_2019)
 
-	for item in playlists['items']:
+	tracks = get_tracks(playlist_id,auth_header)
 
-		if item['owner']['display_name'].lower() == username.lower():
-			print(item['name'])
-			for thing in item:
-				print('\t',thing,item[thing])
-			print()
-			print()
+	for track in tracks:
+		print(track['name'],'|',track['uri'])
